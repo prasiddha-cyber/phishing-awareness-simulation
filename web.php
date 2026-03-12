0a4e43594f4ca1c4765ab0e9df1a9fded833d67b
prasiddha-cyber <prasiddhapingali@gmail.com>
2026-03-12 17:48:54 +0530

    Delete web.php

--- web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PhishingController;
-
-Route::get('/phish/{campaign}', [PhishingController::class, 'showLogin']);
-Route::post('/phish/{campaign}', [PhishingController::class, 'handleLogin']);
